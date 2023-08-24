6582606e7164dc19b5e688ce685c1ae9f401c69f
Chinvoxel <1350106846@qq.com>
2023-08-24 10:52:08 +0800

    chore:第一次基础构建提交

---
+++ README.md
@@ -0,0 +1,2 @@
+## 技术栈
+vue3 + vite + element-plus + js
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <el-row class="mb-4">
+    <el-button @click="handleSubmit">Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+
+  <el-divider /> -->
+
+  <a-space wrap>
+    <a-button type="primary" @click="handleSubmit">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </a-space>
+</template>
+
+<script setup>
+import { getCurrentInstance } from 'vue'
+
+const { proxy } = getCurrentInstance()
+
+const handleSubmit = () => {
+  console.log('proxy: ', proxy)
+}
+</script>
+
+<style scoped></style>
